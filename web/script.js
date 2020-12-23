a051476c292e585585f513f3dfbfd7e48396d060
Kvasik Ivan <64497012+IvanKvasik@users.noreply.github.com>
2020-12-23 21:05:08 +0200

    Update script.js

--- web/script.js
+++ web/script.js
@@ -1,4 +1,10 @@
-let show = (state) => {
-    document.getElementById('modalForm').style.display = state;
-    document.getElementById('filter').style.display = state;
-};
+filter = document.getElementById('filter');
+function show(id) {
+    document.getElementById(id).style.display = 'block';
+    filter.style.display = 'block';
+}
+function hide(){
+	document.getElementById('new').style.display = 'none';
+	document.getElementById('conf').style.display = 'none';
+	filter.style.display = 'none';
+}
