3169940405176586c1351b49c0ce83a2c1759ef1
Georgy Zhilinsky <76434358+GeorgyZhilinsky@users.noreply.github.com>
2020-12-26 20:21:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let mod = document.getElementById('md'),
+	btn = document.getElementById('btc'),
+	fil = document.getElementById('fil');
+btn.onclick = () => {
+	mod.style.display = 'block';
+	fil.style.display = 'block';
+};
